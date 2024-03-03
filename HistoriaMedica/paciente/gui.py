7559f0b384aa0cc0a03a262abe23ca4db9f24136
dasoro <danielsr_1997@hotmail.com>
2024-03-03 00:24:13 -0500

    Entrys y btns

--- HistoriaMedica/paciente/gui.py
+++ HistoriaMedica/paciente/gui.py
@@ -52,4 +52,63 @@ class Frame(tk.Frame):
         self.svNombre = tk.StringVar()
         self.entryNombre = tk.Entry(self,textvariable=self.svNombre)
         self.entryNombre.config(width= 30, font=('ARIAL', 15))
-        self.entryNombre.grid(column=1, row=0 ,padx= 10 ,pady=5)
+        self.entryNombre.grid(column=1, row=0 ,padx= 10 ,pady=5, columnspan=2)
+
+        self.svApePaterno = tk.StringVar()
+        self.entryApePaterno = tk.Entry(self,textvariable=self.svApePaterno)
+        self.entryApePaterno.config(width= 30, font=('ARIAL', 15))
+        self.entryApePaterno.grid(column=1, row=1 ,padx= 10 ,pady=5, columnspan=2)
+
+        self.svApeMaterno = tk.StringVar()
+        self.entryApeMaterno = tk.Entry(self,textvariable=self.svApeMaterno)
+        self.entryApeMaterno.config(width= 30, font=('ARIAL', 15))
+        self.entryApeMaterno.grid(column=1, row=2 ,padx= 10 ,pady=5, columnspan=2)
+
+        self.svDocumento = tk.StringVar()
+        self.entryDocumento = tk.Entry(self,textvariable=self.svDocumento)
+        self.entryDocumento.config(width= 30, font=('ARIAL', 15))
+        self.entryDocumento.grid(column=1, row=3 ,padx= 10 ,pady=5, columnspan=2)
+
+        self.svFecNacimiento = tk.StringVar()
+        self.entryFecNacimiento = tk.Entry(self,textvariable=self.svFecNacimiento)
+        self.entryFecNacimiento.config(width= 30, font=('ARIAL', 15))
+        self.entryFecNacimiento.grid(column=1, row=4 ,padx= 10 ,pady=5, columnspan=2)
+
+        self.svEdad = tk.StringVar()
+        self.entryEdad = tk.Entry(self,textvariable=self.svEdad)
+        self.entryEdad.config(width= 30, font=('ARIAL', 15))
+        self.entryEdad.grid(column=1, row=5 ,padx= 10 ,pady=5, columnspan=2)
+
+        self.svAntecedentes = tk.StringVar()
+        self.entryAntecedentes = tk.Entry(self,textvariable=self.svAntecedentes)
+        self.entryAntecedentes.config(width= 30, font=('ARIAL', 15))
+        self.entryAntecedentes.grid(column=1, row=6 ,padx= 10 ,pady=5, columnspan=2)
+
+        self.svCorreo = tk.StringVar()
+        self.entryCorreo = tk.Entry(self,textvariable=self.svCorreo)
+        self.entryCorreo.config(width= 30, font=('ARIAL', 15))
+        self.entryCorreo.grid(column=1, row=7 ,padx= 10 ,pady=5, columnspan=2)
+
+        self.svPais = tk.StringVar()
+        self.entryPais = tk.Entry(self,textvariable=self.svPais)
+        self.entryPais.config(width= 30, font=('ARIAL', 15))
+        self.entryPais.grid(column=1, row=8 ,padx= 10 ,pady=5, columnspan=2)
+
+        #Buttons
+
+        self.btnNuevo = tk.Button(self, text='Nuevo')
+        self.btnNuevo.config(width=20, font=('ARIAL', 12, 'bold'),
+                            fg='#DAD5D6', bg='#057D00', cursor='hand2',activebackground='#35BD6F')
+        self.btnNuevo.grid(column=0 , row=9, padx=10, pady=5)
+
+        self.btnGuardar = tk.Button(self, text='Guardar')
+        self.btnGuardar.config(width=20, font=('ARIAL', 12, 'bold'),
+                            fg='#DAD5D6', bg='#1658A2', cursor='hand2',activebackground='#5F5F5F')
+        self.btnGuardar.grid(column=1 , row=9, padx=10, pady=5)
+
+        self.btnCancelar = tk.Button(self, text='Cancelar')
+        self.btnCancelar.config(width=20, font=('ARIAL', 12, 'bold'),
+                            fg='#DAD5D6', bg='#EA2B02', cursor='hand2',activebackground='#EC8C77')
+        self.btnCancelar.grid(column=2, row=9, padx=10, pady=5)
+
+
